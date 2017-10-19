7a8f985cec31c5865de7b4d2ed3e96c09decc4ab
Igor Dolzhenkov <igor.dolzhenkov@revolut.com>
2017-10-19 22:40:39 +0300

    Simple price tracker

--- index.js
+++ index.js
@@ -1,52 +1,65 @@
-// http://iss.moex.com/iss/engines/currency/markets/selt/boards/CETS/securities/GBPRUB_TOM.json
-// http://www.moex.com/cs/engines/currency/markets/selt/boardgroups/13/securities/GBPRUB_TOM.hs
-// https://www.tinkoff.ru/api/v1/currency_rates/
-// https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22GBPRUB%22&format=json&env=store://datatables.org/alltableswithkeys
-
 const axios = require('axios')
-
-let previosRate = 0
+const config = require('./config')
 
 function sendMessage(text) {
   axios.post(
-    `https://api.telegram.org/bot{Token}/sendMessage`,
+    `https://api.telegram.org/bot${config.token}/sendMessage`,
     {
       text: text,
-      chat_id: '40196122'
+      chat_id: config.chatId
     }
   )
 }
 
-function getRate() {
-  sendMessage('I am working babe')
-  axios.get('https://www.tinkoff.ru/api/v1/currency_rates/').then(res => {
-    const { data } = res
-    data.payload.rates.map(item => {
-      if (
-        item.category === 'DebitCardsTransfers' &&
-        (item.fromCurrency.name === 'GBP' && item.toCurrency.name === 'RUB')
-      ) {
-        if (previosRate !== item.sell) {
-          d = new Date().toLocaleTimeString()
-          different = (item.sell - previosRate).toFixed(2)
+function extractData(res) {
+  const { data } = res
+  let { payload } = data
+  let priceBuy = payload.prices['buy'].value
+  let priceSell = payload.prices['sell'].value
+  let name = payload.symbol.showName
+  return {
+    name,
+    priceBuy,
+    priceSell
+  }
+}
 
-          if (Math.abs(different) > 0.1) {
-            previosRate = item.sell
-            exp = (item.sell * 400).toFixed(2)
-
-            sendMessage(
-              `Фунты: ${item.sell.toFixed(2)}\t${different}\n${exp}(£400)`
-            )
-            console.log(
-              `[${d}] Купить Фунты сейчас:\t${item.sell.toFixed(
-                2
-              )}\t${different}\t${exp}(£400)`
-            )
-          }
-        }
-      }
-    })
+function getStock(ticker) {
+  axios.get(`https://api.tinkoff.ru/trading/stocks/get?ticker=${ticker}`)
+  .then(res => {
+    let { name, priceBuy, priceSell } = extractData(res)
+    sendMessage(
+      `${name}: Buy:${priceBuy} Sell:${priceSell}`
+    )
   })
 }
 
-setInterval(getRate, 10000)
+
+function notifyByPrice(ticker, {
+  lower, greater
+}) {
+  axios.get(`https://api.tinkoff.ru/trading/stocks/get?ticker=${ticker}`)
+  .then(res => {
+    let { name, priceBuy, priceSell } = extractData(res)
+
+    if ((priceBuy < lower) || (priceSell < lower)){
+      sendMessage(`${name}: Buy:${priceBuy} Sell:${priceSell}`)
+    }
+    if ((priceBuy > greater) || (priceSell > greater)){
+      sendMessage(`${name}: Buy:${priceBuy} Sell:${priceSell}`)
+    }
+  })
+}
+
+setInterval(() => notifyByPrice('V', {
+  lower: 107,
+  greater: 108
+}), 10000)
+setInterval(() => notifyByPrice('MSFT', {
+  lower: 77,
+  greater: 78
+}), 10000)
+setInterval(() => notifyByPrice('ATVI', {
+  lower: 61,
+  greater: 62
+}), 10000)
