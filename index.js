27873cfae0a55b4de486540f9c8813817ca54db3
Igor Dolzhenkov <igor.dolzhenkov@revolut.com>
2017-10-23 00:18:22 +0100

    Enhance the way we trigger tracking

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const axios = require('axios')
 const config = require('./config')
 
+let Stocks = {
+  VISA: 'V',
+  MICRON: 'MU',
+  BLIZZARD: 'ATVI',
+  MICROSOFT: 'MSFT',
+  APPLE: 'AAPL',
+  PAYPALL: 'PYPL',
+  FACEBOOK: 'FB',
+  ELECTRONIC: 'EA',
+  ADOBE: 'ADBE',
+  ALIBABA: 'BABA',
+  DOLLARTREE: 'DLTR',
+  MASTERCARD: 'MA',
+  YY: 'YY',
+  NVIDIA: 'NVDA',
+  WEIBO: 'WB',
+  FIRSTSOLAR: 'FSLR',
+  CHINALODGING: 'HTHT',
+  SOHU: 'SOHU',
+  INTEL: 'INTC',
+  AUTODESK: 'ADSK',
+  TINKOFF: 'TCS',
+  JOBS: 'JOBS',
+  ABBVIE: 'ABBV',
+  ALCOA: 'AA',
+  APPLIED: 'AMAT',
+  REDHAT: 'RHT',
+  HORTON: 'DHI',
+  FMC: 'FMC',
+  ABBOTT: 'ABT',
+  COGNIZANT: 'CTSH'
+}
+
+let defaultInterval = 300000
+
 function sendMessage(text) {
   axios.post(
     `https://api.telegram.org/bot${config.token}/sendMessage`,
@@ -25,6 +60,7 @@ function extractData(res) {
 }
 
 function getStock(ticker) {
+  console.log('getStock '+ ticker)
   axios.get(`https://api.tinkoff.ru/trading/stocks/get?ticker=${ticker}`)
   .then(res => {
     let { name, priceBuy, priceSell } = extractData(res)
@@ -36,30 +72,90 @@ function getStock(ticker) {
 
 
 function notifyByPrice(ticker, {
-  lower, greater
+  buy = {}, sell = {}
 }) {
+  console.log(ticker, buy, sell)
   axios.get(`https://api.tinkoff.ru/trading/stocks/get?ticker=${ticker}`)
   .then(res => {
     let { name, priceBuy, priceSell } = extractData(res)
 
-    if ((priceBuy < lower) || (priceSell < lower)){
+    if ((buy.lower && (priceBuy < buy.lower)) || (sell.lower && (priceSell < sell.lower))){
       sendMessage(`${name}: Buy:${priceBuy} Sell:${priceSell}`)
     }
-    if ((priceBuy > greater) || (priceSell > greater)){
+    if ((buy.greater && (priceBuy > buy.greater)) || (sell.greater && (priceSell > sell.greater))) {
       sendMessage(`${name}: Buy:${priceBuy} Sell:${priceSell}`)
     }
   })
 }
 
-setInterval(() => notifyByPrice('V', {
-  lower: 107,
-  greater: 108
-}), 10000)
-setInterval(() => notifyByPrice('MSFT', {
-  lower: 77,
-  greater: 78
-}), 10000)
-setInterval(() => notifyByPrice('ATVI', {
-  lower: 61,
-  greater: 62
-}), 10000)
+function notifyByPriceInterval({ stock, interval = defaultInterval, buy, sell}) {
+  setInterval(() => notifyByPrice(stock, {
+    buy,
+    sell
+  }), interval)
+}
+
+function notifyByPriceIntervalArray(array) {
+  array.forEach(notifyByPriceInterval)
+}
+
+module.exports = () => {
+  notifyByPriceIntervalArray([
+    {
+      stock: Stocks.VISA,
+      buy: {
+        lower: 107,
+        greater: 108
+      },
+      sell: {
+        lower: 107,
+        greater: 108
+      }
+    },
+    {
+      stock: Stocks.MICRON,
+      buy: {
+        lower: 40,
+        greater: 42
+      }
+    },
+    {
+      stock: Stocks.BLIZZARD,
+      buy: {
+        lower: 61
+      },
+      sell: {
+        greater: 63
+      }
+    },
+    {
+      stock: Stocks.APPLE,
+      buy: {
+        lower: 152,
+        greater: 159
+      }
+    },
+    {
+      stock: Stocks.PAYPALL,
+      buy: {
+        lower: 65,
+        greater: 73
+      }
+    },
+    {
+      stock: Stocks.FACEBOOK,
+      buy: {
+        lower: 165,
+        greater: 180
+      }
+    },
+    {
+      stock: Stocks.ELECTRONIC,
+      buy: {
+        lower: 110,
+        greater: 115
+      }
+    }
+  ])
+}
+
